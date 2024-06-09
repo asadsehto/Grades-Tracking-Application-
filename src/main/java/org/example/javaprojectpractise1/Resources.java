9acdc8d472cab772dd681d15a5330fcc7183e6c2
asgharali2049 <153481137+asgharali2049@users.noreply.github.com>
2024-06-09 13:09:36 +0500

    added pre req section

--- src/main/java/org/example/javaprojectpractise1/Resources.java
+++ src/main/java/org/example/javaprojectpractise1/Resources.java
@@ -7,8 +7,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
+import javafx.scene.Node;
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Objects;
@@ -41,5 +42,64 @@ public class Resources {
         stage.setTitle("HomePage");
         stage.show();
     }
+    @FXML
+    private void handleGradingPolicyButton(ActionEvent event) {
+        try {
+            Parent gradingPage = FXMLLoader.load(getClass().getResource("gradingView.fxml"));
+            Scene gradingScene = new Scene(gradingPage);
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(gradingScene);
+            window.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handlestdAssessment(ActionEvent event){
+        try {
+            Parent gradingPage = FXMLLoader.load(getClass().getResource("stdAssessment.fxml"));
+            Scene gradingScene = new Scene(gradingPage);
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(gradingScene);
+            window.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void closebuttonHandle(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("resources.fxml")));
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Resources");
+        stage.show();
+    }
+
+    @FXML
+    private void CSlink(ActionEvent event) {
+        String url = "https://www.iba-suk.edu.pk/offered-programs/computer-science";
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void handleLinks(ActionEvent event){
+        try {
+            Parent gradingPage = FXMLLoader.load(getClass().getResource("Links.fxml"));
+            Scene gradingScene = new Scene(gradingPage);
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(gradingScene);
+            window.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+
 
